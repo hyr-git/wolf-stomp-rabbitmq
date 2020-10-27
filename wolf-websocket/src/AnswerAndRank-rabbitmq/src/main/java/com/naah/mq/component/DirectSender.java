805cf330f7abfa5914637ce24f24c263d3dcf634
wert <>
2020-10-27 13:31:07 +0800

    修改项目名称

---
+++ wolf-websocket/src/AnswerAndRank-rabbitmq/src/main/java/com/naah/mq/component/DirectSender.java
@@ -0,0 +1,28 @@
+package com.naah.mq.component;
+
+import java.util.Date;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+
+/***
+ * 生产者
+ * @author m1832
+ *
+ */
+@Component
+public class DirectSender {
+	
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+	
+	public void sendMsg(String msg) {
+		String sengMsg = "hello, " + new Date() +"------"+msg;
+		System.out.println("DirectSender: "+sengMsg);
+		rabbitTemplate.convertAndSend("WORK_ORDER_QUEUE_NAME",sengMsg);
+	}
+}
+
