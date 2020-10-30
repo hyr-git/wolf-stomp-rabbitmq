14086a2c7f9c4405e7ca5b0972ad73c65fc0b24d
wert <>
2020-10-30 19:49:53 +0800

    需要注意此处使用rabbitmq作为消息代理,不能使用：      *    stomp中订阅的消息为/topic/toWeb.[userId]      *    服务器端发送消息也必须为/topic/toWeb.[userId]

--- wolf-stomp/src/main/java/com/naah/stomp/rabbitmq/Receiver.java
+++ wolf-stomp/src/main/java/com/naah/stomp/rabbitmq/Receiver.java
@@ -34,14 +34,21 @@ public class Receiver {
 
    /* @Autowired
     RecordService recordService;*/
-
+    /****
+     *
+     * 需要注意此处使用rabbitmq作为消息代理,不能使用：
+     *    stomp中订阅的消息为/topic/toWeb.[userId]
+     *    服务器端发送消息也必须为/topic/toWeb.[userId]
+     */
     @RabbitHandler
     @RabbitListener(queues = RabbitConfig.TO_WEB_QUEUE_NAME)
     public void processToWeb(@Payload MQMessage message,@Headers Map<String,Object> header ) throws IOException {
         System.out.println("processToWeb  : " + JSON.toJSONString(message));
     	String toUserId = message.getToUserId();
     	toUserId = "16033333333";
-        messagingTemplate.convertAndSend( "/"+RabbitConfig.TO_WEB_QUEUE_NAME+"/"+toUserId, message);
+        //messagingTemplate.convertAndSend( "/"+RabbitConfig.TO_WEB_QUEUE_NAME+"/"+toUserId, JSON.toJSONString(message));
+    	messagingTemplate.convertAndSend( "/topic/"+RabbitConfig.TO_WEB_QUEUE_NAME+"."+toUserId, JSON.toJSONString(message));
+        
       	/***
       	 * 
       	 * /user/16033333333/toWeb
@@ -60,7 +67,7 @@ public class Receiver {
        System.out.println("processToAPP  : " + JSON.toJSONString(message));
        String toUserId = message.getToUserId();
        // amqpTemplate.convertAndSend( destination, context);
-        messagingTemplate.convertAndSend( "/"+RabbitConfig.TO_APP_QUEUE_NAME, message);
+        messagingTemplate.convertAndSend( "/"+RabbitConfig.TO_APP_QUEUE_NAME+"/"+toUserId, message);
         List<String> tagsList = new ArrayList<>();
     	tagsList.add(toUserId);
     	tagsList.add("17088888888");
