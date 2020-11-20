5849328a626976acdd6e8579b79152b7dd369ea2
maliujiang <maliujiang@carsir.xin>
2020-11-20 17:51:07 +0800

    私信队列、延时队列处理

--- wolf-rabbitmq/src/main/java/com/naah/mq/dlx/RabbitDLXConsumer.java
+++ wolf-rabbitmq/src/main/java/com/naah/mq/dlx/RabbitDLXConsumer.java
@@ -17,16 +17,18 @@ import lombok.extern.slf4j.Slf4j;
 
 /**
  * RabbitMQ 消息消费者
+ *
+ * @author huan.fu
  * @date 2018/8/13 - 15:39
  */
 @Slf4j
-public class RabbitOrderConsumer {
+public class RabbitDLXConsumer {
 
 	private static final String IP_ADDRESS = "127.0.0.1";
 	private static final String USER = "admin";
 	private static final String PASSWD = "admin";
 
-	private static String ORDER_EXCHANGE_NAME = "order_exchange";
+	private static String DLX_QUEUE_NAME = "dlx.queue";
 	
 	private static final int PORT = 5672;
 
@@ -45,24 +47,15 @@ public class RabbitOrderConsumer {
 			Channel channel = connection.createChannel();
 			
 			// 消费端消息限流。
-			/*
-			 * 设置客户端最多接收未被ack的消息个数, 只有消息 手动签收  此参数才会生效。
+			/* 设置客户端最多接收未被ack的消息个数, 只有消息 手动签收  此参数才会生效。
 			 * 设置消费批量投递数目,一次性投递64条消息,当消费者未确认消息累计达到64条,rabbitMQ将不会向此channel上的消费者投递消息,直到为确认数小于64条再投递
 			 * @param prefetchCount 投递数目
              * @param global 是否针对整个Channel。true表示此投递数是给Channel设置的，false是给Channel上的Consumer设置的。
              * @throws java.io.IOException if an error is encountered
 			 */
-			//整个管道最多64条
 			channel.basicQos(64,true);
 			
 			Consumer consumer = new DefaultConsumer(channel) {
-				/**consumer处理消息
-				 * @param consumerTag 消息标签
-				 * @param enevlope 消息的包装数据
-				 * @param properties 消息的内容参数
-				 * @param body 消息对象的body 
-				 * @throws IOException
-				 */
 				@Override
 				public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
 					log.info(">>>>>>>>接收到消息：" + new String(body, StandardCharsets.UTF_8));
@@ -76,18 +69,15 @@ public class RabbitOrderConsumer {
 			* 开始一个非局部、非排他性消费， with a server-generated consumerTag.
 			* 执行这个方法会回调handleConsumeOk方法
 			* @param queue 队列名称
-			* @param autoAck 是否自动应答。
-			*        false表示consumer在成功消费过后必须要手动回复一下服务器，
-			*        如果不回复，服务器就将认为此条消息消费失败，继续分发给其他consumer。
+			* @param autoAck 是否自动应答。false表示consumer在成功消费过后必须要手动回复一下服务器，如果不回复，服务器就将认为此条消息消费失败，继续分发给其他consumer。
 			* @param callback 回调方法类，一般为自己的Consumer类
 			* @return 由服务器生成的consumertag
 			* @throws java.io.IOException if an error is encountered
 			*/
-			channel.basicConsume(ORDER_EXCHANGE_NAME,false, consumer);
+			channel.basicConsume(DLX_QUEUE_NAME,false, consumer);
 			TimeUnit.SECONDS.sleep(10000000L);
-			
-			channel.close();
-		    connection.close();
+			   channel.close();
+		        connection.close();
 		}
 	}
 }
--- wolf-rabbitmq/src/main/java/com/naah/mq/dlx/RabbitOrderProducer.java
+++ wolf-rabbitmq/src/main/java/com/naah/mq/dlx/RabbitOrderProducer.java
@@ -5,15 +5,14 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.TimeoutException;
 
+import com.rabbitmq.client.AMQP.Exchange;
 import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.MessageProperties;
-import com.rabbitmq.client.AMQP.Exchange;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,8 +21,6 @@ import lombok.extern.slf4j.Slf4j;
  * <p>
  * 死信队列和延时消息的使用
  * 消息先到 order_queue 中，然后 10s 钟没有消费，消息流转到死信队列 dlx.queue 中
- *
- * @author huan.fu
  * @date 2018/10/15 - 14:10
  */
 @Slf4j
@@ -60,8 +57,18 @@ public class RabbitOrderProducer {
 			 */
 			 channel = connection.createChannel();
 			
+			 //设置消息确认机制
+			 channel.confirmSelect();
+			 
 			/*
 			 * 2、初始化topic类型的持久化非自动删除的交换器,指定交换器的名称、类型、是否持久化、是否自动删除
+			 * 声明一个队列不存在,则会被创建
+			 * @param exchange  队列名称 
+			 * @param type  exchange type：direct、fanout、topic、headers 
+			 * @param durable 持久化：true队列会在重启过后存在(队列的消息需要单独设置持久化)
+			 * @param autoDelete 是否自动删除(没有连接自动删除)
+			 * @param arguments 队列的其他属性(构造参数)
+			 * @return Queue.Declare：宣告队列的声明确认方法已成功声明
 			 */
 			Exchange.DeclareOk declareOk = channel.exchangeDeclare(ORDER_EXCHANGE_NAME, BuiltinExchangeType.TOPIC, true, false, null);
 		
@@ -74,16 +81,16 @@ public class RabbitOrderProducer {
 			//arguments.put("x-dead-letter-routing-key", "为 dlx exchange 指定路由键，如果没有特殊指定则使用原队列的路由键");
 				
 			/**
-			 * 
 			 * 3、初始化队列--持久化、非排他、非自动删除
 			 *   指定队列名称
-			 *   durable 是否持久化,队列的声明默认是放在内存中的,重启之后会丢失;若持久化会保存在erlang自带的mnesia数据库中
-			 *   exclusive 是否排他,一般有2个作用,a当连接关闭的时候该队列是否自动删除;
+			 * @param queueName 队列名称 
+			 * @param durable 是否持久化,队列的声明默认是放在内存中的,重启之后会丢失;若持久化会保存在erlang自带的mnesia数据库中
+			 * @param exclusive 是否排他,一般有2个作用,a当连接关闭的时候该队列是否自动删除;
 			 *       b是该队列是否是私有的private,若不是排外，可以使用2个消费者都访问同一个队列,
 			 *       否则排外会对当前队列加锁,其他通道channel是不能访问的，强制访问会报错。
 			 *       一般等于true用于一个队列只能有一个消费者来消费的场景。 
-			 *   autoDelete当最后一个消费者断开连接之后是否自动删除队列 
-			 *   arguments 参数集合
+			 * @param  autoDelete当最后一个消费者断开连接之后是否自动删除队列 
+			 * @param  arguments 参数集合
 			 *     x-expires：当队列在指定的时间没有被访问就会被删除；
 			 *     x-max-length：用于限定队列消息的最大值长度,超过指定的长度会把最早的几条删除；
 			 *     x-message-ttl: 设置队列中所有消息的生存周期,时间到了消息会被删除；
@@ -99,11 +106,18 @@ public class RabbitOrderProducer {
 			
 			/*
 			 * 4、队列绑定交换器
+			 * 将队列绑定到Exchange，不需要额外的参数。
+			 * @param queue 队列名称
+			 * @param exchange 交换机名称
+			 * @param routingKey 路由关键字
+			 * @return Queue.BindOk：如果成功创建绑定，则返回绑定确认方法。
+			 * @throws java.io.IOException if an error is encountered
 			 */
 			channel.queueBind(ORDER_QUEUE_NAME, ORDER_EXCHANGE_NAME, ORDER_ROUTING_KEY);
+			
 			String message = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + " 创建订单.";
 
-			// 创建死信交换器和队列
+			//创建死信交换器和队列
 			channel.exchangeDeclare(DLX_EXCHANGE_NAME, BuiltinExchangeType.TOPIC, true, false, null);
 			channel.queueDeclare(DLX_QUEUE_NAME, true, false, false, null);
 			channel.queueBind(DLX_QUEUE_NAME, DLX_EXCHANGE_NAME, ORDER_ROUTING_KEY);
@@ -112,8 +126,37 @@ public class RabbitOrderProducer {
 			//AMQP.BasicProperties.Builder properties = new AMQP.BasicProperties().builder().expiration("6000");
 			//channel.basicPublish(orderExchangeName, "order.save", properties.build(), message.getBytes(StandardCharsets.UTF_8));
 			
-		    //TODO 为指定该队列设置所有的过期时间
-			channel.basicPublish(ORDER_EXCHANGE_NAME, "order.save", MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes(StandardCharsets.UTF_8));
+		    /*
+		     * TODO 
+		     * 发布一条不用持久化的消息，且设置两个监听。
+		     * @param exchange 消息交换机名称,空字符串将使用直接交换器模式，发送到默认的Exchange=amq.direct。此状态下，RoutingKey默认和Queue名称相同
+		     * @param routingKey 路由关键字
+		     * @param mandatory 监听是否有符合的队列,
+		     *       当mandatory为true时，exchange根据自身类型和消息routeKey无法找到符合条件的queue，那么会调用basic.return方法将消息返回给生产者（Basic.Return + Content-Header + Content-Body）；
+		     *       当mandatory为false时，broker会直接将消息扔掉。 
+		     * @param immediate 监听符合的队列上是有至少一个Consumer,
+		     *       当immediate为true时，如果exchange在将消息路由到queue(s)时对应的queue上没有消费者，那么这条消息不会放入队列中。当与消息routeKey关联的所有queue（一个或者多个）都没有消费者时，该消息会通过basic.return方法返还给生产者
+		     * @param BasicProperties 设置消息持久化：MessageProperties.PERSISTENT_TEXT_PLAIN是持久化；MessageProperties.TEXT_PLAIN是非持久化。
+		     * @param body 消息对象转换的byte[]
+		     * 
+		     * @备注说明
+		     *  1、当mandatory为true时,如果exchange根据自身类型和消息routeKey无法找到符合条件的queue，
+		     *     那么会调用basic.return方法将消息返回给生产者（Basic.Return + Content-Header + Content-Body）；
+		     *     当mandatory设置为false时，出现上述情形broker会直接将消息扔掉。
+		     *     
+		     *  2、当immediate为true时,若exchange在将消息路由到queue(s)时对应的queue上没有消费者，
+		     *  那么这条消息不会放入队列中。当与消息routingKey关联的所有queue（一个或者多个）都没有消费者时，该消息会通过basic.return方法返还给生产者。
+             * 
+             * @概括来说，
+             *     mandatory标志告诉服务器至少将该消息route到一个队列中，否则将消息返还给生产者；
+             *     immediate标志告诉服务器如果该消息关联的queue上有消费者，则马上将消息投递给它，
+             *     如果所有queue都没有消费者，直接把消息返还给生产者，不用将消息入队列等待消费者了。
+             * 
+             * 注意：在RabbitMQ3.0以后的版本里，去掉了immediate参数的支持，发送带immediate=true标记的publish会返回如下错误：
+             *       com.rabbitmq.client.AlreadyClosedException: connection is already closed due to connection error;protocol method: #method<connection.close>(reply-code=540, reply-text=NOT_IMPLEMENTED - immediate=true, class-id=60, method-id=40)。
+             * 为什么取消支持：immediate标记会影响镜像队列性能，增加代码复杂性，并建议采用“TTL”和“DLX”等方式替代。
+		     */
+			channel.basicPublish(ORDER_EXCHANGE_NAME, "order.save",true,false, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes(StandardCharsets.UTF_8));
 
 			log.info("消息发送完成......");
 			
