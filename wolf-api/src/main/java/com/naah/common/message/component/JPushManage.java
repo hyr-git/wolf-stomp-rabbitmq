e4085e68dc4fe550d4f655bbbb28598546ecc801
maliujiang <maliujiang@carsir.xin>
2021-01-06 14:08:12 +0800

    添加公用方法

--- wolf-api/src/main/java/com/naah/common/message/component/JPushManage.java
+++ wolf-api/src/main/java/com/naah/common/message/component/JPushManage.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
 
 import com.alibaba.fastjson.JSONObject;
